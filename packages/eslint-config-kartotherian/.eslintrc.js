abacd045f97e1826f3d530302d7978c62a0514b4
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2018-03-08 03:35:56 -0500

    0.0.4 some common settings

--- packages/eslint-config-kartotherian/.eslintrc.js
+++ packages/eslint-config-kartotherian/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  "extends": "airbnb-base"
+  extends: 'airbnb-base',
+  env: {
+    node: true,
+    es6: true,
+    browser: false,
+  },
+  rules: {
+    'no-underscore-dangle': [0],
+    'no-plusplus': [0],
+
+    // Allow ForOfStatement
+    'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+  },
 };
